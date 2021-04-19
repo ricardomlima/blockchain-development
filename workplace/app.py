f2ef2e37809d73b694fa1a2ce30fbefa8b7bc602
Ricardo M. Lima <ricardolima89@gmail.com>
2021-04-19 16:18:05 -0300

    Documenting each step of mining

--- workplace/app.py
+++ workplace/app.py
@@ -15,11 +15,24 @@ def get_chain():
 
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
+    """
+    The blockchain is public. In this case everyone has access
+    to the chain. If you want to mine it you can.
+    """
+
+    # Access the last item in the chain
     previous_block = blockchain.get_previous_block()
+
+    # This is what you need for mining
     previous_proof = previous_block['proof']
+
+    # This is the actual mining. Tryig to solve a problem.
     proof = blockchain.proof_of_worf(previous_proof)
+
+    # with a proof at hands now you are good to go on creating a new block
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
+
     response = {
         'message': 'Congratulations on mining a new block!',
         'index': block['index'],
