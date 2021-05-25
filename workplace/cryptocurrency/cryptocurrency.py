2875d1eb0c29121f10f3fb3d240f9f7109653776
Ricardo M. Lima <ricardolima89@gmail.com>
2021-05-25 19:13:30 +0000

    Adding transactions to a block

--- workplace/cryptocurrency/cryptocurrency.py
+++ workplace/cryptocurrency/cryptocurrency.py
@@ -8,6 +8,8 @@ from flask import Flask
 class Blockchain:
     def __init__(self):
         self.chain = []
+        # A blockchain starts with zero transactions
+        self.transactions = []
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
@@ -16,7 +18,10 @@ class Blockchain:
             'timestamp': str(datetime.datetime.now()),
             'previous_hash': previous_hash,
             'proof': proof,
+            'transactions': self.transactions,
         }
+        # Clean transactions after creating a block
+        self.transactions = []
         self.chain.append(block)
         return block
 
@@ -59,3 +64,8 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender, 'receiver': receiver, 'amount': amount})
+        previous_block = self.get_previous_block()
+        return previous_block["index"] + 1
