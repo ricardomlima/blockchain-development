ac542da0916b1547fa12073694b75b22eef6cf88
Ricardo M. Lima <ricardolima89@gmail.com>
2021-06-27 20:59:02 -0300

    endpoint to update chain

--- workplace/app.py
+++ workplace/app.py
@@ -90,6 +90,22 @@ def connect_node():
     return jsonify(response), 201
 
 
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {
+            "message": "The chain was replaced",
+            "new_chain": blockchain.chain
+        }
+    else:
+        response = {
+            "message": "The chain was not replaced",
+            "chain": blockchain.chain
+        }
+    return jsonify(response), 200
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, as!'
