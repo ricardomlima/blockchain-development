3c2e4a86410da63d2bde90e75188a25695718235
Ricardo M. Lima <ricardolima89@gmail.com>
2021-09-21 15:06:29 -0300

    smart contract function signature

---
+++ workplace/smart_contract/hadcoins_ico.sol
@@ -0,0 +1,22 @@
+// ICO Hadcoins
+
+/*
+ Run on http://remix.ethereum.org/#appVersion=0.7.7&optimize=false&version=soljson-v0.4.11+commit.68ef5810.js
+ MyEtherWaller == 3.11.2.4
+*/
+
+
+// version
+pragma solidity ^0.4.11
+
+contract hadcoin_ico {
+    // max number of hadcoins available in ICO
+    uint public max_hadcoins = 1000000;
+
+    // dolar cost
+    uint public usd_to_hadcoins = 1000;
+
+    // total hadcoins bought by investors
+    uint public total_hadcoins_bought = 0;
+
+}
